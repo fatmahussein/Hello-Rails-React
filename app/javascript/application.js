f6be8eccb3401f51558c3d3e5b3e0b3e668e8db6
Fatma Hussein <fartunhussein346@gmail.com>
2023-12-07 11:43:19 +0300

    update application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { Provider } from "react-redux";
-import App from "./components/app";
+import App from "./components/App"
 import store from "./components/store";
 
 function Index() {
